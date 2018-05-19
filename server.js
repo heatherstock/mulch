81dc9ffcca7568d7289057bf9735c4e15bcad72e
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-05-19 16:06:13 +0100

    :construction: Connect to database. Require mongoose and config file

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
 
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url)
+.then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to Mulch"});
 });
